221cceee38ead981ce86019fba0953aa35213778
Jacob Reimers <Jacob Reimers@SIF>
2011-08-13 10:11:35 +0100

    Created RestContext, RestQueryable and RestQueryProvider. Moved existing files into folders.

---
+++ UrlQueryParser.Tests/Provider/RestQueryProviderTests.cs
@@ -0,0 +1,23 @@
+﻿namespace UrlQueryParser.Tests.Provider
+{
+	using System;
+	using System.Linq;
+	using System.Linq.Expressions;
+
+	using NUnit.Framework;
+
+	using UrlQueryParser.Provider;
+
+	public class RestQueryProviderTests
+	{
+		private readonly RestContext<string> _provider;
+
+		public RestQueryProviderTests() { _provider = new RestContext<string>(); }
+
+		[Test]
+		public void CanProcessExpression()
+		{
+			var result = _provider.Query.OfType<string>().Where(x => x.Length <= 3).Skip(1).Take(1).Count();
+		}
+	}
+}
---
+++ UrlQueryParser/Provider/RestContext.cs
@@ -0,0 +1,17 @@
+﻿namespace UrlQueryParser.Provider
+{
+	using System.Linq;
+
+	public class RestContext<T>
+	{
+		public RestContext() { }
+
+		public IQueryable Query
+		{
+			get
+			{
+				return new RestQueryable<T>();
+			}
+		}
+	}
+}
