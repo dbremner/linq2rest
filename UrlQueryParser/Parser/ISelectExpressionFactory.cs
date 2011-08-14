c10ba605aa907acf813e7d78807daf36fe021d9f
Jacob Reimers <Jacob Reimers@TIR>
2011-08-14 18:15:45 +0100

    Reorganized files.

---
+++ UrlQueryParser/Parser/ISelectExpressionFactory.cs
@@ -0,0 +1,14 @@
+// (c) Copyright Reimers.dk.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993] for details.
+// All other rights reserved.
+
+namespace UrlQueryParser.Parser
+{
+	using System;
+
+	public interface ISelectExpressionFactory<T>
+	{
+		Func<T, object> Create(string selection);
+	}
+}
