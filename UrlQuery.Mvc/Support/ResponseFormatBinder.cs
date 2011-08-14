49cba2ecd1b2eeebd446f5cb62caacce4b19e82b
Jacob Reimers <Jacob Reimers@TIR>
2011-08-14 18:06:06 +0100

    Added copyright text.

--- UrlQuery.Mvc/Support/ResponseFormatBinder.cs
+++ UrlQuery.Mvc/Support/ResponseFormatBinder.cs
@@ -1,4 +1,9 @@
-﻿namespace UrlQuery.Mvc.Support
+﻿// (c) Copyright Reimers.dk.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993] for details.
+// All other rights reserved.
+
+namespace UrlQuery.Mvc.Support
 {
 	using System;
 	using System.Collections.Generic;
