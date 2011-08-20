0ab9f77bb4add729498c3a620f95bb0988f30825
Jacob Reimers <Jacob Reimers@localhost>
2011-08-20 16:10:06 +0100

    Added build scripts. Removed project packages. Packages should be restored using the RestorePackages.ps1 script.

---
+++ Build.ps1
@@ -0,0 +1,28 @@
+param(
+[string]$apiKey = "Your API Key",
+$configuration = "Release"
+)
+
+Write-Host $apiKey
+
+function UpdatePackages
+{
+	$packageConfigs = Get-ChildItem -Path .\ -Include "packages.config" -Recurse
+	foreach($config in $packageConfigs){
+		nuget i $config.FullName -o packages -x
+	}
+}
+
+function BuildSolution
+{
+	msbuild .\Linq2Rest.sln /p:configuration=$configuration
+}
+
+function PublishPackage
+{
+	nuget pack Linq2Rest.nuspec
+}
+
+UpdatePackages
+BuildSolution
+PublishPackage
