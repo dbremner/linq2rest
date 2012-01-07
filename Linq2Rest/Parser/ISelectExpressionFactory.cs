827e334e262fc5a11ae05f93e5e2edde4f8477b8
Jacob Reimers <Jacob Reimers@localhost>
2012-01-07 09:49:23 +0000

    Added support for arithmetic operations not enclosed in precedence group.

--- Linq2Rest/Parser/ISelectExpressionFactory.cs
+++ Linq2Rest/Parser/ISelectExpressionFactory.cs
@@ -6,14 +6,12 @@
 namespace Linq2Rest.Parser
 {
 	using System;
-	using System.Diagnostics.Contracts;
 	using System.Linq.Expressions;
 
 	/// <summary>
 	/// Defines the public interface for a SelectExpressionFactory.
 	/// </summary>
 	/// <typeparam name="T">The <see cref="Type"/> to create expression for.</typeparam>
-	[ContractClass(typeof(SelectionExpressionFactoryContracts<>))]
 	public interface ISelectExpressionFactory<T>
 	{
 		/// <summary>
@@ -22,16 +20,5 @@ namespace Linq2Rest.Parser
 		/// <param name="selection">The properties to select.</param>
 		/// <returns>An instance of a <see cref="Func{T1,TResult}"/>.</returns>
 		Expression<Func<T, object>> Create(string selection);
-	}
-
-	[ContractClassFor(typeof(ISelectExpressionFactory<>))]
-	internal abstract class SelectionExpressionFactoryContracts<T> : ISelectExpressionFactory<T>
-	{
-		public Expression<Func<T, object>> Create(string selection)
-		{
-			Contract.Ensures(Contract.Result<Expression<Func<T, object>>>() != null);
-
-			throw new NotImplementedException();
-		}
 	}
 }
