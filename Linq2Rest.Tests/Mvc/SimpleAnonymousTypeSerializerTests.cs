dfd71dbfc828e89262202729748cb9633d779164
Jacob Reimers <Jacob Reimers@localhost>
2012-01-16 21:53:37 +0000

    Added tests.

---
+++ Linq2Rest.Tests/Mvc/SimpleAnonymousTypeSerializerTests.cs
@@ -0,0 +1,35 @@
+﻿// (c) Copyright Reimers.dk.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://www.opensource.org/licenses/MS-PL] for details.
+// All other rights reserved.
+
+namespace Linq2Rest.Tests.Mvc
+{
+	using System;
+	using System.Linq;
+	using Linq2Rest.Mvc.Provider;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class SimpleAnonymousTypeSerializerTests
+	{
+		[Test]
+		public void CanDeserializeAnonymousTypeWithTwoProperties()
+		{
+			const string Json = "{\"Title\":\"blah\", \"Value\":2}";
+			var source = new[] { new Tuple<string, int>("test", 1), };
+			var anonymousType = source.Select(x => new { Title = x.Item1, Value = x.Item2 }).First();
+
+			var serializerType = typeof(SimpleAnonymousTypeSerializer<>).MakeGenericType(anonymousType.GetType());
+			var serializer = Activator.CreateInstance(serializerType);
+
+			var deserializeMethod = serializerType
+				.GetMethods()
+				.First(m => m.Name == "Deserialize" && m.ReturnType == anonymousType.GetType());
+
+			dynamic result = deserializeMethod.Invoke(serializer, new object[] { Json });
+
+			Assert.AreEqual("blah", result.Title);
+		}
+	}
+}
