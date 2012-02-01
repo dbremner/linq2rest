078e5eb1e6be5b9e0427f3c0202e85ddc3ad262d
Jacob Reimers <Jacob Reimers@localhost>
2012-02-01 21:26:50 +0000

    Added documentation and code contracts.

--- Linq2Rest/Parser/SortExpressionFactory.cs
+++ Linq2Rest/Parser/SortExpressionFactory.cs
@@ -13,15 +13,31 @@ namespace Linq2Rest.Parser
 	using System.Linq.Expressions;
 	using System.Web.UI.WebControls;
 
+	/// <summary>
+	/// Defines the SortExpressionFactory
+	/// </summary>
 	public class SortExpressionFactory : ISortExpressionFactory
 	{
 		private static readonly CultureInfo DefaultCulture = CultureInfo.GetCultureInfo("en-US");
 
+		/// <summary>
+		/// Creates an enumeration of sort descriptions from its string representation.
+		/// </summary>
+		/// <param name="filter">The string representation of the sort descriptions.</param>
+		/// <typeparam name="T">The <see cref="Type"/> of item to sort.</typeparam>
+		/// <returns>An <see cref="IEnumerable{T}"/> if the passed sort descriptions are valid, otherwise null.</returns>
 		public IEnumerable<SortDescription<T>> Create<T>(string filter)
 		{
 			return Create<T>(filter, DefaultCulture);
 		}
 
+		/// <summary>
+		/// Creates an enumeration of sort descriptions from its string representation.
+		/// </summary>
+		/// <param name="filter">The string representation of the sort descriptions.</param>
+		/// <param name="formatProvider">The <see cref="IFormatProvider"/> to use when reading the sort descriptions.</param>
+		/// <typeparam name="T">The <see cref="Type"/> of item to sort.</typeparam>
+		/// <returns>An <see cref="IEnumerable{T}"/> if the passed sort descriptions are valid, otherwise null.</returns>
 		public IEnumerable<SortDescription<T>> Create<T>(string filter, IFormatProvider formatProvider)
 		{
 			if (string.IsNullOrWhiteSpace(filter))
