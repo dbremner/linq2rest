66589f2d7600a4f419e0731321562cc0f8b5183e
Jacob Reimers <Jacob Reimers@localhost>
2012-04-01 20:37:45 +0100

    Added tests.

--- Linq2Rest.Rx/IAsyncRestClient.cs
+++ Linq2Rest.Rx/IAsyncRestClient.cs
@@ -2,10 +2,24 @@ namespace Linq2Rest.Rx
 {
 	using System;
 
+	/// <summary>
+	/// Defines the public interface for an async REST client.
+	/// </summary>
 	public interface IAsyncRestClient
 	{
+		/// <summary>
+		/// Begins the REST request.
+		/// </summary>
+		/// <param name="callback">The <see cref="AsyncCallback"/> to invoke when response is received.</param>
+		/// <param name="state">The asynchronous state object.</param>
+		/// <returns>An <see cref="IAsyncResult"/> instance.</returns>
 		IAsyncResult BeginGetResult(AsyncCallback callback, object state);
 
+		/// <summary>
+		/// Gets the result from the passed <see cref="IAsyncResult"/>.
+		/// </summary>
+		/// <param name="result">The async operation result.</param>
+		/// <returns>The downloaded resource as a <see cref="string"/>.</returns>
 		string EndGetResult(IAsyncResult result);
 	}
 }
--- Linq2Rest.Rx/RestObservable.cs
+++ Linq2Rest.Rx/RestObservable.cs
@@ -9,6 +9,10 @@
 	using System.Threading.Tasks;
 	using Linq2Rest.Provider;
 
+	/// <summary>
+	/// Defines an observable REST query.
+	/// </summary>
+	/// <typeparam name="T">The <see cref="Type"/> of object returned by the REST service.</typeparam>
 	public class RestObservable<T> : IQbservable<T>
 	{
 		private readonly IAsyncRestClientFactory _restClient;
@@ -16,6 +20,11 @@
 		private readonly List<IObserver<T>> _observers = new List<IObserver<T>>();
 		private readonly IAsyncExpressionProcessor _processor;
 
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="RestObservable{T}"/> class.
+		/// </summary>
+		/// <param name="restClient">The <see cref="IAsyncRestClientFactory"/> to create a web client.</param>
+		/// <param name="serializerFactory">The <see cref="ISerializerFactory"/> to create the serializer.</param>
 		public RestObservable(IAsyncRestClientFactory restClient, ISerializerFactory serializerFactory)
 		{
 			_processor = new AsyncExpressionProcessor(new Provider.ExpressionVisitor()); // new ExpressionProcessor(new Provider.ExpressionVisitor());
@@ -31,6 +40,9 @@
 			Expression = expression;
 		}
 
+		/// <summary>
+		/// Gets the type of the element(s) that are returned when the expression tree associated with this instance of IQbservable is executed.
+		/// </summary>
 		public Type ElementType
 		{
 			get
@@ -39,14 +51,27 @@
 			}
 		}
 
+		/// <summary>
+		/// Gets the expression tree that is associated with the instance of IQbservable.
+		/// </summary>
 		public Expression Expression { get; private set; }
 
+		/// <summary>
+		/// Gets the query provider that is associated with this data source.
+		/// </summary>
 		public IQbservableProvider Provider { get; private set; }
 
+		/// <summary>
+		/// Notifies the provider that an observer is to receive notifications.
+		/// </summary>
+		/// <returns>
+		/// A reference to an interface that allows observers to stop receiving notifications before the provider has finished sending them.
+		/// </returns>
+		/// <param name="observer">The object that is to receive notifications.</param>
 		public IDisposable Subscribe(IObserver<T> observer)
 		{
 			_observers.Add(observer);
-			var filter = (MethodCallExpression)Expression;
+			var filter = Expression as MethodCallExpression;
 			var parameterBuilder = new ParameterBuilder(_restClient.ServiceBase);
 
 			_processor.ProcessMethodCall(filter, parameterBuilder, GetResults, GetIntermediateResults)
