bb43fa9c65a3a562b54f1bc2a11247780f431662
Jacob Reimers <Jacob Reimers@Loke>
2012-04-09 19:54:07 +0100

    Added manifest file. Updated version.

--- Linq2Rest.Reactive.WinRT/Properties/AssemblyInfo.cs
+++ Linq2Rest.Reactive.WinRT/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Linq2Rest.Reactive.WinRT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generates OData style Rx Observables from LINQ queries.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Reimers.dk")]
-[assembly: AssemblyProduct("Linq2Rest.Reactive.WinRT")]
+[assembly: AssemblyProduct("Linq2Rest.Reactive")]
 [assembly: AssemblyCopyright("Copyright © Reimers.dk 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
