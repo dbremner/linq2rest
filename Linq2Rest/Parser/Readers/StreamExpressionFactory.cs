3c8c5def35cbb3584595e0661d16c866ba51a5ce
Jacob Reimers <Jacob Reimers@localhost>
2012-04-21 20:06:37 +0100

    Added suppression justification.

--- Linq2Rest/Parser/Readers/StreamExpressionFactory.cs
+++ Linq2Rest/Parser/Readers/StreamExpressionFactory.cs
@@ -19,7 +19,7 @@ namespace Linq2Rest.Parser.Readers
 			}
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Cannot dispose here.")]
 		public override ConstantExpression Convert(string token)
 		{
 			var baseResult = base.Convert(token);
