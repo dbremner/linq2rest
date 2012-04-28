691f8ed8e60f2adcf585df8f13813df9d9f53283
Jacob Reimers <Jacob Reimers@localhost>
2012-04-28 19:01:53 +0100

    Changed WP7 demo to query Netflix.

--- Linq2Rest.Reactive.WP7Sample/MainPage.xaml.cs
+++ Linq2Rest.Reactive.WP7Sample/MainPage.xaml.cs
@@ -6,7 +6,10 @@
 namespace Linq2Rest.Reactive.WP7Sample
 {
 	using System;
+	using System.Collections.ObjectModel;
 	using System.Reactive.Linq;
+	using Linq2Rest.Reactive.WP7Sample.Models;
+	using Linq2Rest.Reactive.WP7Sample.Support;
 	using Microsoft.Phone.Controls;
 
 	public partial class MainPage : PhoneApplicationPage
@@ -15,17 +18,22 @@ namespace Linq2Rest.Reactive.WP7Sample
 		public MainPage()
 		{
 			Loaded += MainPage_Loaded;
+			Resources.Add("Films", new ObservableCollection<NetflixFilm>());
 			InitializeComponent();
 		}
 
 		void MainPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
 		{
-			var observable = new RestObservable<SampleDto>(new FakeAsyncRestClientFactory(), new PhoneSerializerFactory());
+			var films = Resources["Films"] as ObservableCollection<NetflixFilm>;
+			// http://odata.netflix.com/v2/Catalog/Titles
+			var observable = new RestObservable<NetflixFilm>(
+				new AsyncJsonRestClientFactory(new Uri("http://odata.netflix.com/v2/Catalog/Titles")),
+				new ODataSerializerFactory());
 			var subscription = observable
-				.Where(x => x.Text != "blah")
+				.Where(x => x.Name.Contains("harry"))
 				.Subscribe(
-						   x => txtStatus.Text = "Got one",
-						   ex => txtStatus.Text = "Uh oh",
+						   x => Dispatcher.BeginInvoke(() => films.Add(x)),
+						   ex => Dispatcher.BeginInvoke(() => txtStatus.Text = ex.Message),
 						   () => Dispatcher.BeginInvoke(() => txtStatus.Text = "Finished"));
 		}
 	}
