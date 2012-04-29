cefda498d942cac11c2ca5674562a54938fff369
Jacob Reimers <Jacob Reimers@localhost>
2012-04-29 11:11:19 +0100

    Updated Netflix search demo.

--- Linq2Rest.Reactive.WinRT.Sample/BlankPage.xaml.cs
+++ Linq2Rest.Reactive.WinRT.Sample/BlankPage.xaml.cs
@@ -1,25 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using Linq2Rest.Reactive.Implementations;
 using Linq2Rest.Reactive.WinRT.Sample.Models;
 using Linq2Rest.Reactive.WinRT.Sample.Support;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Linq2Rest.Reactive.WinRT.Sample
 {
@@ -31,7 +19,6 @@ namespace Linq2Rest.Reactive.WinRT.Sample
 		public BlankPage()
 		{
 			InitializeComponent();
-			Resources.Add("FilmList", new ObservableCollection<NetflixFilm>());
 		}
 
 		/// <summary>
@@ -41,28 +28,32 @@ namespace Linq2Rest.Reactive.WinRT.Sample
 		/// property is typically used to configure the page.</param>
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
-			var filmList = Resources["FilmList"] as ObservableCollection<NetflixFilm>;
-			films.ItemsSource = filmList;
-			var observable =
-				new RestObservable<NetflixFilm>(
-					new AsyncJsonRestClientFactory(
-						new Uri("http://odata.netflix.com/v2/Catalog/Titles")),
-					new ODataSerializerFactory());
-
-			observable
-				.Where(x => x.Name.Contains("harry") && x.BoxArt.MediumUrl != null)
-				.Subscribe(x => Dispatcher.InvokeAsync(
-					CoreDispatcherPriority.Normal,
-					AddFilm,
-					this,
-					x));
 		}
 
 		private void AddFilm(object sender, InvokedHandlerArgs e)
 		{
-			var filmList = Resources["FilmList"] as ObservableCollection<NetflixFilm>;
 			var film = e.Context as NetflixFilm;
-			filmList.Add(film);
+			films.Items.Add(film);
+		}
+
+		private void OnSearch(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			var button = (sender as Button);
+			button.IsEnabled = false;
+			if (films.Items.Any())
+			{
+				films.Items.Clear();
+			}
+
+			var query = search.Text;
+			new RestObservable<NetflixFilm>(
+					new AsyncJsonRestClientFactory(
+						new Uri("http://odata.netflix.com/v2/Catalog/Titles")),
+					new ODataSerializerFactory())
+				.Where(x => x.Name.Contains(query))
+				.Subscribe(
+				x => Dispatcher.InvokeAsync(CoreDispatcherPriority.Normal, AddFilm, this, x),
+				() => Dispatcher.InvokeAsync(CoreDispatcherPriority.Normal, (s, ea) => button.IsEnabled = true, this, null));
 		}
 	}
 }
