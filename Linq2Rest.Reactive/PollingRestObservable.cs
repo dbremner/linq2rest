02c4732f7b39bdef45433cc7f8f5552be854a6a0
Jacob Reimers <Jacob Reimers@localhost>
2012-05-21 21:52:06 +0100

    Moved classes to separate files.

--- Linq2Rest.Reactive/PollingRestObservable.cs
+++ Linq2Rest.Reactive/PollingRestObservable.cs
@@ -3,8 +3,6 @@
 // Please see http://www.opensource.org/licenses/MS-PL] for details.
 // All other rights reserved.
 
-using System.Reactive;
-
 namespace Linq2Rest.Reactive
 {
 	using System;
@@ -64,85 +62,6 @@ namespace Linq2Rest.Reactive
 				.Select(x => Observable.FromAsyncPattern<Stream>(client.BeginGetResult, client.EndGetResult)())
 				.Select(x => x.Select(serializer.DeserializeList))
 				.SelectMany(x => x);
-		}
-	}
-
-	internal class TriggeredRestObservable<T> : InnerRestObservableBase<T>
-	{
-		private readonly IObservable<Unit> _trigger;
-		private readonly IQbservableProvider _provider;
-		private IDisposable _internalSubscription;
-		private IDisposable _subscribeSubscription;
-
-		internal TriggeredRestObservable(
-			IObservable<Unit> trigger,
-			IAsyncRestClientFactory restClient,
-			ISerializerFactory serializerFactory,
-			Expression expression,
-			IScheduler subscriberScheduler,
-			IScheduler observerScheduler)
-			: base(restClient, serializerFactory, expression, subscriberScheduler, observerScheduler)
-		{
-			_trigger = trigger;
-			_provider = new TriggeredRestQueryableProvider(trigger, restClient, serializerFactory, subscriberScheduler, observerScheduler);
-		}
-
-		/// <summary>
-		/// Gets the query provider that is associated with this data source.
-		/// </summary>
-		public override IQbservableProvider Provider
-		{
-			get { return _provider; }
-		}
-
-		public override IDisposable Subscribe(IObserver<T> observer)
-		{
-			Observers.Add(observer);
-			_subscribeSubscription = SubscriberScheduler
-				.Schedule(
-						  observer,
-						  (s, o) =>
-						  {
-							  _internalSubscription = _trigger
-								  .Select(
-								  x =>
-								  {
-									  var filter = Expression as MethodCallExpression;
-									  var parameterBuilder = new ParameterBuilder(RestClient.ServiceBase);
-
-									  return Processor.ProcessMethodCall(
-										  filter,
-										  parameterBuilder,
-										  GetResults,
-										  GetIntermediateResults);
-								  })
-									  .SelectMany(y => y)
-									  .Subscribe(new ObserverPublisher(Observers, ObserverScheduler));
-						  });
-			return new RestSubscription(observer, Unsubscribe);
-		}
-
-		private void Unsubscribe(IObserver<T> observer)
-		{
-			if (!Observers.Contains(observer))
-			{
-				return;
-			}
-
-			Observers.Remove(observer);
-			observer.OnCompleted();
-			if (Observers.Count == 0)
-			{
-				if (_internalSubscription != null)
-				{
-					_internalSubscription.Dispose();
-				}
-
-				if (_subscribeSubscription != null)
-				{
-					_subscribeSubscription.Dispose();
-				}
-			}
 		}
 	}
 }
--- Linq2Rest.Reactive/TriggeredRestQueryableProvider.cs
+++ Linq2Rest.Reactive/TriggeredRestQueryableProvider.cs
@@ -2,33 +2,33 @@
 // This source is subject to the Microsoft Public License (Ms-PL).
 // Please see http://www.opensource.org/licenses/MS-PL] for details.
 // All other rights reserved.
-
 namespace Linq2Rest.Reactive
 {
 	using System;
 	using System.Linq.Expressions;
+	using System.Reactive;
 	using System.Reactive.Concurrency;
 	using System.Reactive.Linq;
 	using Linq2Rest.Provider;
 
-	internal class PollingRestQueryableProvider : RestQueryableProviderBase
+	internal class TriggeredRestQueryableProvider : RestQueryableProviderBase
 	{
-		private readonly TimeSpan _frequency;
+		private readonly IObservable<Unit> _trigger;
 
-		public PollingRestQueryableProvider(
-			TimeSpan frequency,
+		public TriggeredRestQueryableProvider(
+			IObservable<Unit> trigger,
 			IAsyncRestClientFactory asyncRestClient,
 			ISerializerFactory serializerFactory,
 			IScheduler subscriberScheduler,
 			IScheduler observerScheduler)
 			: base(asyncRestClient, serializerFactory, subscriberScheduler, observerScheduler)
 		{
-			_frequency = frequency;
+			_trigger = trigger;
 		}
 
 		protected override IQbservable<TResult> CreateQbservable<TResult>(Expression expression, IScheduler subscriberScheduler, IScheduler observerScheduler)
 		{
-			return new PollingRestObservable<TResult>(_frequency, AsyncRestClient, SerializerFactory, expression, subscriberScheduler, observerScheduler);
+			return new TriggeredRestObservable<TResult>(_trigger, AsyncRestClient, SerializerFactory, expression, subscriberScheduler, observerScheduler);
 		}
 	}
 }
