2a1e2ede2e41d3e9f1c08fd1113af6a396394869
Jacob Reimers <Jacob Reimers@localhost>
2012-07-19 20:09:34 +0100

    Issue 14 Added test to verify ordering by child property.

--- Linq2Rest.Tests/Parser/SortExpressionFactoryTests.cs
+++ Linq2Rest.Tests/Parser/SortExpressionFactoryTests.cs
@@ -26,9 +26,9 @@ namespace Linq2Rest.Tests.Parser
 		{
 			_items = new[]
 				{
-					new FakeItem { IntValue = 2, DoubleValue = 5 },
-					new FakeItem { IntValue = 1, DoubleValue = 4 },
-					new FakeItem { IntValue = 3, DoubleValue = 4 }
+					new FakeItem { IntValue = 2, DoubleValue = 5, StringValue = "aa" },
+					new FakeItem { IntValue = 1, DoubleValue = 4, StringValue = "a" },
+					new FakeItem { IntValue = 3, DoubleValue = 4, StringValue = "aaa" }
 				};
 		}
 
@@ -88,6 +88,20 @@ namespace Linq2Rest.Tests.Parser
 			Assert.AreEqual(3, sortedItems.OfType<FakeItem>().ElementAt(0).IntValue);
 			Assert.AreEqual(1, sortedItems.OfType<FakeItem>().ElementAt(1).IntValue);
 			Assert.AreEqual(2, sortedItems.OfType<FakeItem>().ElementAt(2).IntValue);
+		}
+
+		[Test]
+		public void WhenOrderingByChildPropertyThenUsesChildProperty()
+		{
+			const string Orderstring = "StringValue/Length desc";
+
+			var descriptions = _factory.Create<FakeItem>(Orderstring);
+			var filter = new ModelFilter<FakeItem>(x => true, null, descriptions, 0, -1);
+			var sortedItems = filter.Filter(_items).ToArray();
+
+			Assert.AreEqual("aaa", sortedItems.OfType<FakeItem>().ElementAt(0).StringValue);
+			Assert.AreEqual("aa", sortedItems.OfType<FakeItem>().ElementAt(1).StringValue);
+			Assert.AreEqual("a", sortedItems.OfType<FakeItem>().ElementAt(2).StringValue);
 		}
 	}
 }
