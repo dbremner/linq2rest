93c2968623727753b9c6d57e1d06b7284b080028
Jacob Reimers <Jacob Reimers@localhost>
2012-10-27 11:34:21 +0200

    Updated copyright text.

--- Linq2Rest/Provider/ODataExpressionConverter.cs
+++ Linq2Rest/Provider/ODataExpressionConverter.cs
@@ -1,4 +1,4 @@
-﻿//  Copyright © Reimers.dk 2011
+﻿//  Copyright © Reimers.dk 2012
 //	This source is subject to the Microsoft Public License (Ms-PL).
 //	Please see http://go.microsoft.com/fwlink/?LinkID=131993] for details.
 //	All other rights reserved.
