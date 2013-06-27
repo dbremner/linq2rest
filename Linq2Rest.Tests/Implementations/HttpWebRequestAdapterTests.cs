3cefd5bcf3c1e814c8acafc15dc483bf3436d673
Mark Rucker <rucker.mark@gmail.com>
2013-06-27 18:38:45 -0500

    Fixing a few small bugs. Adding several tests

---
+++ Linq2Rest.Tests/Implementations/HttpWebRequestAdapterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Linq2Rest.Implementations;
+using Linq2Rest.Provider;
+
+namespace Linq2Rest.Tests.Implementations
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class HttpWebRequestAdapterTests
+    {
+        [Test]
+        public void CanCreateTwoHttpPostRequestsAndWriteToThemWithoutBlowingUp()
+        {
+            //Couldn't figure out a good way to verify the contents of the request stream. Perhaps there is
+            //Probably a way to send the request back to yourself and verify the request contents that way.
+            //Not really worth it for such a small amount of testing
+
+            var httpRequestFactory = new HttpRequestFactory();
+
+// ReSharper disable InconsistentNaming
+            var uri_1          = new Uri("http://test.com");
+            var responseMime_1 = "text/xml";
+            var requestMime_1  = "text/json";
+
+            var uri_2          = new Uri("http://test.com");
+            var responseMime_2 = "text/xml";
+            var requestMime_2  = "text/html";
+
+            IHttpRequest httpRequest_1 = httpRequestFactory.Create(uri_1
+                                                                  , HttpMethod.Post
+                                                                  , responseMime_1
+                                                                  , requestMime_1);
+
+            IHttpRequest httpRequest_2 = httpRequestFactory.Create(uri_2
+                                                                  , HttpMethod.Post
+                                                                  , responseMime_2
+                                                                  , requestMime_2);
+
+            var httpWebRequestAdapter_1 = (HttpWebRequestAdapter)httpRequest_1;
+
+            var httpWebRequestAdapter_2 = (HttpWebRequestAdapter)httpRequest_2;
+
+            var expectedRequest_1 = "{ 'value' : 'expected request' }";
+            var expectedRequest_2 = "<html>expected request</html>";
+
+            httpWebRequestAdapter_1.WriteRequestStream(new MemoryStream(Encoding.ASCII.GetBytes(expectedRequest_1)));
+            httpWebRequestAdapter_2.WriteRequestStream(new MemoryStream(Encoding.ASCII.GetBytes(expectedRequest_2)));
+// ReSharper restore InconsistentNaming
+        }
+    }
+}
--- Linq2Rest/Provider/IHttpRequestFactory.cs
+++ Linq2Rest/Provider/IHttpRequestFactory.cs
@@ -21,23 +21,23 @@ namespace Linq2Rest.Provider
     public interface IHttpRequestFactory
     {
         /// <summary>
-        /// Creates an IHttpRequest that can be used to send an httpRequest
+        /// Creates an IHttpRequest that can be used to send an http request
         /// </summary>
         /// <param name="uri">The location the request is to be sent to</param>
         /// <param name="method">The method to use to send the request</param>
-        /// <param name="acceptMimeType">The Mime type we accept in response</param>
+        /// <param name="responseMimeType">The Mime type we accept in response</param>
         /// <param name="requestMimeType">The Mime type we are sending in request</param>
         /// <returns>The HttpRequest we are creating</returns>
-        IHttpRequest Create(Uri uri, HttpMethod method, string acceptMimeType, string requestMimeType);
+        IHttpRequest Create(Uri uri, HttpMethod method, string responseMimeType, string requestMimeType);
     }
 
     [ContractClassFor(typeof(IHttpRequestFactory))]
     internal abstract class HttpRequestFactoryContracts:IHttpRequestFactory
     {
-        public IHttpRequest Create(Uri uri, HttpMethod method, string acceptMimeType, string requestMimeType = null)
+        public IHttpRequest Create(Uri uri, HttpMethod method, string responseMimeType, string requestMimeType)
         {
             Contract.Requires<ArgumentNullException>(uri != null);
-            Contract.Requires<ArgumentNullException>(acceptMimeType != null);
+            Contract.Requires<ArgumentNullException>(responseMimeType != null);
             Contract.Requires<ArgumentException>(method != HttpMethod.None);
 
             throw new NotImplementedException();
