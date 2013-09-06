6b9d8e1de61db95376d5950e70d10034ec397dd8
Jacob Reimers <Jacob Reimers@localhost>
2013-09-06 22:02:41 +0200

    Updated to version 4.0.0.0

--- Linq2Rest.Reactive.SL.IntegrationTests/Properties/AssemblyInfo.cs
+++ Linq2Rest.Reactive.SL.IntegrationTests/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 //      Revision
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
