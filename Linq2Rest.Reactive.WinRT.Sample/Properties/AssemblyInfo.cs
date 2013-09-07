f00cd231cf12ba3abcda5a6dce63859cf8cf9c69
Jacob Reimers <Jacob Reimers@localhost>
2013-09-07 08:25:12 +0200

    Reverted to version 3.3.1.0. No breaking public API change.

--- Linq2Rest.Reactive.WinRT.Sample/Properties/AssemblyInfo.cs
+++ Linq2Rest.Reactive.WinRT.Sample/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Linq2Rest.Reactive.WinRT.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Reimers.dk")]
 [assembly: AssemblyProduct("Linq2Rest.Reactive.WinRT.Sample")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Reimers.dk 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("3.3.1.0")]
+[assembly: AssemblyFileVersion("3.3.1.0")]
 [assembly: ComVisible(false)]
