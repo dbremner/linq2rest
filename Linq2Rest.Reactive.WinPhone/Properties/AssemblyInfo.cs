2b82a778022a1e75f74dbea7e07ca6ff72c0be56
Jacob Reimers <Jacob Reimers@localhost>
2013-11-30 13:52:54 +0100

    Updated to version 3.3.3.0

--- Linq2Rest.Reactive.WinPhone/Properties/AssemblyInfo.cs
+++ Linq2Rest.Reactive.WinPhone/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 //      Revision
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.2.0")]
-[assembly: AssemblyFileVersion("3.3.2.0")]
+[assembly: AssemblyVersion("3.3.3.0")]
+[assembly: AssemblyFileVersion("3.3.3.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
