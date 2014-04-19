09be2503f608e08e5361edfb3f1ecefe48c8ff77
Jacob Reimers <Jacob Reimers@localhost>
2014-04-19 18:02:15 +0200

    Updated dependencies. Handle WinRT code.

--- Linq2Rest.Reactive.WinRT.Sample/Support/ODataSerializerFactory.cs
+++ Linq2Rest.Reactive.WinRT.Sample/Support/ODataSerializerFactory.cs
@@ -20,5 +20,16 @@ namespace Linq2Rest.Reactive.WinRT.Sample.Support
 		{
 			return new ODataSerializer<T>();
 		}
+
+		/// <summary>
+		/// Creates an instance of an <see cref="ISerializer{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">The item type for the serializer.</typeparam>
+		/// <typeparam name="TSource">The item type to provide alias metadata for the serializer.</typeparam>
+		/// <returns>An instance of an <see cref="ISerializer{T}"/>.</returns>
+		public ISerializer<T> Create<T, TSource>()
+		{
+			return new ODataSerializer<T>();
+		}
 	}
 }
